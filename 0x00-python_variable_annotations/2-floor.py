35d7d6804f5f19d1214113cebbf1c39195fdbadc
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-12 03:34:29 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' A Python Module '''
+
+
+def floor(n: float) -> int:
+    ''' function takes in a float and returns an int '''
+    import math
+    return math.floor(n)
+
