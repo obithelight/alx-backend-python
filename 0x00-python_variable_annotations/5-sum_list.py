17ab1613ce95b9e7018377480c4f77446230887d
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-12 03:59:05 +0100

    sum of list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' This Module defines a function `sum_list` '''
+from typing import List
+
+
+def sum_list(input_list: list[float]) -> float:
+    ''' returns the sum of list elements '''
+    return float(sum(input_list))
