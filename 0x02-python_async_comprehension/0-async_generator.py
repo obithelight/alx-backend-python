10c7cd46bbc97b3f4bfef5785146f3ffc357b3af
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-16 03:18:53 +0100

    asynchronous generators

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+''' A Coroutine Async Generator '''
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''
+    Loops 10 times asynchronously and yields a random number between 0 and 10
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
