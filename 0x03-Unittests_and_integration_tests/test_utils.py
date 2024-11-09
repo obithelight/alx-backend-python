017ea7b2246346248c9f1ae11246c1218b09dc35
Obi Chukwu <kennetho.onu@gmail.com>
2024-11-09 03:18:06 +0100

    implemented tests for advanced tasks

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,33 +1,81 @@
 #!/usr/bin/env python3
-""" This Python3 module tests the utils package """
+""" Defines test class for utils.access_nested_map function """
 
-import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from typing import Dict, Tuple, Union
+import unittest
+import utils
+from unittest.mock import Mock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """
-    Test case class for the access_nested_map function.
-    """
+    """ Provides functionality for testing utils.access_nested_map """
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": 1}, ("a"), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, nestedMap, path, expectedResult):
-        """ Test the access_nested_map function with diff. input scenarios """
-        self.assertEqual(access_nested_map(nestedMap, path), expectedResult)
+        ])
+    def test_access_nested_map(self,
+                               nested_map: Dict,
+                               path: Tuple[str],
+                               expected: Union[Dict, int]
+                               ) -> None:
+        """ Performs unit tests on utils.access_nested_map """
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",), 'a'),
-        ({"a": 1}, ("a", "b"), 'b')
-    ])
-    def test_access_nested_map_exception(self, nested_map, path, expected):
-        """ Test that a KeyError is raised for diff. inputs """
-        with self.assertRaises(KeyError) as e:
-            access_nested_map(nested_map, path)
-        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+        ({}, ("a"), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+        ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Dict,
+                                         path: Tuple[str],
+                                         exception: Exception
+                                         ) -> None:
+        """ Tests that utils.access_nested_map returns expected o/p """
+        with self.assertRaises(exception):
+            utils.access_nested_map(nested_map, path)
 
 
+class TestGetJson(unittest.TestCase):
+    """ Tests that utils.get_json returns expected result """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    def test_get_json(self,
+                      test_url: str,
+                      test_payload: Dict
+                      ) -> Dict:
+        """ Tests that utils.get_json returns expected output """
+        mocked_attrs = {"json.return_value": test_payload}
+        with patch("requests.get", return_value=Mock(**mocked_attrs)) as res:
+            self.assertEqual(utils.get_json(test_url), test_payload)
+            res.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Provides functionality to test utils.memoize """
+
+    def test_memoize(self) -> None:
+        """ Tests utils.memoize """
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @utils.memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(
+                          TestClass,
+                          "a_method",
+                          return_value=lambda: 42
+                          ) as memoized_fn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memoized_fn.assert_called_once()
