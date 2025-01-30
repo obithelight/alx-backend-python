6b3c7fd9acd44ecd7064f680c2990665390371cd
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-01-30 13:23:27 +0100

    advanced task - 1

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,18 @@
 from typing import Sequence, Any, Union
 
 
-# The types of the elements of the input are not know
+# The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Retrieves the first element from the input list 'lst' safely.
+
+    Args:
+        lst (Sequence[Any]): The input list.
+
+    Returns:
+        Union[Any, None]: The first element of the list or 'None' if
+        the list is empty.
+    """
     if lst:
         return lst[0]
     else:
