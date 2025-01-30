3c7e3e3909ea3ac3584d31fc2e69e44bae760b94
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-01-30 13:25:50 +0100

    advanced task - 1

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,5 +1,7 @@
-from typing import Sequence, Any, Union
+#!/usr/bin/env python3
+''' A Python3 Module '''
 
+from typing import Sequence, Any, Union
 
 # The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
